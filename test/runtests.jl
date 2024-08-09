049890f3ba9240c435a493a6b288e4d6931b742c
Ola Yeku <ooyeku@gmail.com>
2024-08-08 21:26:59 -0500

    add tests for fetching headline data

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,9 @@
 include("../src/StockPrice.jl")
+include("../src/Headlines.jl")
 using Test
 using YFinance
 using DataFrames
+
 
 # Test if data fetching works
 ticker = "AAPL"
@@ -14,3 +16,16 @@ data = get_prices(ticker, range="1mo", interval="1d") |> DataFrame
 test_prices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 expected_sma_5 = [3, 4, 5, 6, 7, 8]
 @test StockPrice.compute_sma(test_prices, 5) == expected_sma_5
+
+
+# Test if headlines fetching works
+symbols = ["AAPL", "GOOGL", "MSFT"]
+headlines = Headlines.get_headlines_dataframe(symbols)
+
+@test !isempty(headlines[!, :title])
+@test !isempty(headlines[!, :publisher])
+@test !isempty(headlines[!, :link])
+@test !isempty(headlines[!, :timestamp])
+@test !isempty(headlines[!, :symbols])
+
+
